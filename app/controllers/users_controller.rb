cdedfe58048a2353a0be0d53e25ec68ce9ab0837
kaito0112 <123792716+kaito0112@users.noreply.github.com>
2023-02-23 19:06:21 +0000

    3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,27 @@
 class UsersController < ApplicationController
-  
+  before_action :authenticate_user!
+
   def index
+    @users = User.all
+    @book = Book.new
   end
-  
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path(@book.id)
+  end
+
   def show
   end
-  
+
   def edit
+  end
+
+  private
+
+  def user_params
+    params.require(:book).permit(:name, :introduction, :profile_image)
   end
 end
